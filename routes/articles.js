64bd756e671b1101be8c4835844b700c0c814671
ansariumar <umar.ansari7331@gmail.com>
2022-05-24 23:54:01 +0530

    now the save button after edititing the article works too

--- routes/articles.js
+++ routes/articles.js
@@ -30,30 +30,39 @@ router.get('/:slug', async (req,res) => {
 	
 })
 
-// The request that comes is the Article object that have a {title, description,markdown}
-router.post('/', async (req,res) => {
 
-	let newArticle = new Article({				
-		title: req.body.title,
-		description: req.body.description,
-		markdown: req.body.markdown
-	})
-		
-	try{
-		newArticle = await newArticle.save().catch((err) => console.log(err));	
-		console.log(newArticle);
-		res.redirect(`/articles/${newArticle.slug}`);
-	}catch(e){
-		console.log('lol1')
-	  	res.render('articles/new', {article: newArticle})	//When there is an error
-	}
+router.post('/', async (req, res, next) => {
+	req.article = new Article()
+	next()
+  }, saveArticleAndRedirect('new'))
 
-})
+
+router.put('/:id', async (req, res, next) => {
+	req.article = await Article.findById(req.params.id);
+	next(); 	
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req,res) => {
 	const deleted_blog = await Article.findByIdAndDelete(req.params.id);
 	console.log(`The Article with title"${deleted_blog.title}" was deleted`); 
 	res.redirect('/');
 })
+
+ function saveArticleAndRedirect(path) {
+	return async (req, res) => {
+		let article = req.article;
+
+		article.title = req.body.title;
+		article.description = req.body.description;
+		article.markdown = req.body.markdown;
+		console.log(article);
+		try {
+			article = await article.save().catch((err) => console.log(err));	//the try dosn't worked and always was sent tp the catch block the error was in the "article.save()" some speling mistake or you forgot either async or await
+			res.redirect(`/articles/${article.slug}`)
+		} catch (e) {
+			res.render(`articles/${path}`, {article: article});
+		}
+	}
+}
 
 module.exports = router;
